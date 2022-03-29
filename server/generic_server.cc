4069030ad9b4d2cb23ee67acb23793a322a1c7fd
Nadeem <nadeem.shehzad@desy.de>
2022-03-29 15:45:54 +0200

    initial commit. First iteration.

---
+++ server/generic_server.cc
@@ -0,0 +1,45 @@
+#include <ChimeraTK/ApplicationCore/ApplicationCore.h>
+#include <ChimeraTK/ApplicationCore/PeriodicTrigger.h>
+#include <ChimeraTK/ApplicationCore/EnableXMLGenerator.h>
+
+namespace ctk = ChimeraTK;
+
+struct GenericApp : public ctk::Application {
+  GenericApp() : Application("generic_server") {  
+    ChimeraTK::setDMapFilePath("devices.dmap");
+  }
+  ~GenericApp() { shutdown(); }
+  ctk::ConfigReader config{this, "Config", getName() + "_configuration.xml", {}};
+  std::vector<ctk::PeriodicTrigger> periodicTriggers;
+  struct ConnectingDeviceModuleGroup : public ctk::ModuleGroup{
+    ConnectingDeviceModuleGroup(ctk::EntityOwner* owner, std::string alias,
+      std::string triggerPath, std::string pathInDevice, std::string initScript) :
+      ModuleGroup(owner, alias, ""),
+      _deviceModule(this, alias, triggerPath, nullptr, pathInDevice) {}
+    ctk::ConnectingDeviceModule _deviceModule;
+  }; 
+  std::vector<ConnectingDeviceModuleGroup> connectingDeviceModules;
+  void defineConnections() override;
+};
+static GenericApp theGenericApp;
+
+
+void GenericApp::defineConnections() {
+  
+  std::vector<std::string> timers = config.get<std::vector<std::string>>("periodicTimers");
+  for (const std::string& timer : timers) {
+    periodicTriggers.emplace_back(ctk::PeriodicTrigger(this, timer, "Periodic timer", config.get<uint32_t>(timer+"/period")));
+  }
+   
+  std::vector<std::string> devices = config.get<std::vector<std::string>>("devices");
+  for (const std::string& device : devices) {
+    connectingDeviceModules.emplace_back(this, device,
+    config.get<std::string>(device+"/triggerPath"),
+    config.get<std::string>(device+"/pathInDevice"),
+    config.get<std::string>(device+"/initScript")
+    );
+  }
+  
+  Application::defineConnections();
+  //dumpConnections();
+}
