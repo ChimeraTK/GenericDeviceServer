0b17dea74e6a66749ce145c8a0c13f2c5c1498aa
Patrick Nonn <patrick.nonn@desy.de>
2022-05-23 17:25:31 +0200

    - Changed PROJECT_BASE_NAME to "generic-device", to remove the references to doocs, and chimeratk, which could lead to misunderstandings\n- Changed project name to "generic-device-server" and adjusted filenames in "sample_configs/" and the source code accordingly

--- server/generic_server.cc
+++ server/generic_server.cc
@@ -6,11 +6,11 @@
 namespace ctk = ChimeraTK;
 
 struct GenericApp : public ctk::Application {
-  GenericApp() : Application("generic_server") {
+  GenericApp() : Application("generic_device_server") {
     ChimeraTK::setDMapFilePath("devices.dmap");
   }
   ~GenericApp() { shutdown(); }
-  ctk::ConfigReader config{this, "Config", getName() + "_configuration.xml", {}};
+  ctk::ConfigReader config{this, "Config","generic-device-server_configuration.xml", {}};
   std::vector<ctk::PeriodicTrigger> periodicTriggers;
   struct ConnectingDeviceModuleGroup : public ctk::ModuleGroup{
     ConnectingDeviceModuleGroup(ctk::EntityOwner* owner, std::string alias,
