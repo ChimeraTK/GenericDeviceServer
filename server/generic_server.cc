4fb84410fea59615452e2fa4b9d820b256418393
Martin Killenberg <martin.killenberg@desy.de>
2022-06-03 16:54:04 +0200

    fixed names for sample config
    Doocs config now working out of the box

--- server/generic_server.cc
+++ server/generic_server.cc
@@ -6,44 +6,38 @@
 namespace ctk = ChimeraTK;
 
 struct GenericApp : public ctk::Application {
-  GenericApp() : Application("generic_chimeratk_server") {
-    ChimeraTK::setDMapFilePath("devices.dmap");
-  }
+  GenericApp() : Application("generic_chimeratk_server") { ChimeraTK::setDMapFilePath("devices.dmap"); }
   ~GenericApp() { shutdown(); }
   ctk::ConfigReader config{this, "Config", getName() + "_configuration.xml", {}};
   std::vector<ctk::PeriodicTrigger> periodicTriggers;
-  struct ConnectingDeviceModuleGroup : public ctk::ModuleGroup{
-    ConnectingDeviceModuleGroup(ctk::EntityOwner* owner, std::string alias,
-      std::string triggerPath, std::string pathInDevice, std::string initScript) :
-      ModuleGroup(owner, alias, ""),
-      _deviceModule(this, alias, triggerPath, nullptr, pathInDevice) {
-        if (!initScript.empty()) {
-          initHandler = std::make_unique<ctk::ScriptedInitHandler> (this, "", "", initScript, _deviceModule.getDeviceModule());
-        }
+  struct ConnectingDeviceModuleGroup : public ctk::ModuleGroup {
+    ConnectingDeviceModuleGroup(ctk::EntityOwner* owner, std::string alias, std::string triggerPath,
+        std::string pathInDevice, std::string initScript)
+    : ModuleGroup(owner, alias, ""), _deviceModule(this, alias, triggerPath, nullptr, pathInDevice) {
+      if(!initScript.empty()) {
+        initHandler =
+            std::make_unique<ctk::ScriptedInitHandler>(this, "", "", initScript, _deviceModule.getDeviceModule());
       }
+    }
     ctk::ConnectingDeviceModule _deviceModule;
-    std::unique_ptr <ctk::ScriptedInitHandler> initHandler;
+    std::unique_ptr<ctk::ScriptedInitHandler> initHandler;
   };
   std::vector<ConnectingDeviceModuleGroup> connectingDeviceModules;
   void defineConnections() override;
 };
 static GenericApp theGenericApp;
 
-
 void GenericApp::defineConnections() {
-
   std::vector<std::string> timers = config.get<std::vector<std::string>>("periodicTimers");
-  for (const std::string& timer : timers) {
-    periodicTriggers.emplace_back(ctk::PeriodicTrigger(this, timer, "Periodic timer", config.get<uint32_t>(timer+"/period")));
+  for(const std::string& timer : timers) {
+    periodicTriggers.emplace_back(
+        ctk::PeriodicTrigger(this, timer, "Periodic timer", config.get<uint32_t>(timer + "/period")));
   }
 
   std::vector<std::string> devices = config.get<std::vector<std::string>>("devices");
-  for (const std::string& device : devices) {
-    connectingDeviceModules.emplace_back(this, device,
-    config.get<std::string>(device+"/triggerPath"),
-    config.get<std::string>(device+"/pathInDevice"),
-    config.get<std::string>(device+"/initScript")
-    );
+  for(const std::string& device : devices) {
+    connectingDeviceModules.emplace_back(this, device, config.get<std::string>(device + "/triggerPath"),
+        config.get<std::string>(device + "/pathInDevice"), config.get<std::string>(device + "/initScript"));
   }
 
   Application::defineConnections();
