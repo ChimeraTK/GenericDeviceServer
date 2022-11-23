32b607182f25e334d5c903a8d9ee2160fedfa221
Martin Killenberg <martin.killenberg@desy.de>
2022-11-23 17:46:08 +0100

    run clang format on test

--- tests/writeWordStatus.cc
+++ tests/writeWordStatus.cc
@@ -1,10 +1,9 @@
 #include <ChimeraTK/Device.h>
 
-int main (){
-  ChimeraTK::setDMapFilePath ( "devices.dmap" );
+int main() {
+  ChimeraTK::setDMapFilePath("devices.dmap");
 
   ChimeraTK::Device d("TMCB1");
   d.open();
-  d.write("/BSP/WORD_STATUS/DUMMY_WRITEABLE",42);
-
+  d.write("/BSP/WORD_STATUS/DUMMY_WRITEABLE", 42);
 }
