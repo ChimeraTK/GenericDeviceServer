d57e489379dee57e25ed8f948b39585a1bb30a92
Martin Killenberg <martin.killenberg@desy.de>
2022-11-23 17:37:34 +0100

    use init executable in example config

---
+++ tests/writeWordStatus.cc
@@ -0,0 +1,10 @@
+#include <ChimeraTK/Device.h>
+
+int main (){
+  ChimeraTK::setDMapFilePath ( "devices.dmap" );
+
+  ChimeraTK::Device d("TMCB1");
+  d.open();
+  d.write("/BSP/WORD_STATUS/DUMMY_WRITEABLE",42);
+
+}
