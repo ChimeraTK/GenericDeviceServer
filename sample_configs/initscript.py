3c7e0a77149ff1940de951ce970cb0664b29e5bf
Dietrich Rothe <dietrich.rothe@desy.de>
2023-07-10 11:22:14 +0200

    use RegisterAccessor in init script
    as on demo slides.
    Functionality stays same

--- sample_configs/initscript.py
+++ sample_configs/initscript.py
@@ -1,15 +1,22 @@
 #! /usr/bin/python3
 
-import mtca4u
+import deviceaccess as da
+import numpy as np
+
+da.setDMapFilePath('devices.dmap')
+d = da.Device('ADC_BOARD')
+d.open()
+
+acc = d.getOneDRegisterAccessor(np.uint32, 
+    'BSP/SCRATCH')
+acc[0] = 42
+acc.write()
+
+
+# ------------- not shown in talk ----------
 import os
 
-mtca4u.set_dmap_location("devices.dmap")
-dev = mtca4u.Device("ADC_BOARD")
-
-dev.write("BSP", "SCRATCH", 42)
-
-
-fid = dev.read("BSP", "ID")
+fid = d.read("BSP/ID")
 if fid != 0:
     # Forward to CG's script for real hw initialization
     err=os.system("./main.py -t initialize -s 3 -c dwc8vm1")
